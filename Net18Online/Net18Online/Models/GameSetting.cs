eaafee65cf664642a4af3a226dbca22b2b82aaa3
proninp <velninpapro@gmail.com>
2024-09-17 15:31:48 +0300

    HomeWork01 - Added Settings Launcher and config file - [09.17]

---
+++ Net18Online/Net18Online/Models/GameSetting.cs
@@ -0,0 +1,7 @@
+﻿namespace Net18Online.Models;
+public class GameSetting
+{
+    public int GuessAttempts { get; set; } = 5;
+    public int MaxNumber { get; set; } = 50;
+    public int MinNumber { get; set; } = 1;
+}
