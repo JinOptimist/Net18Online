3f242bc78ae62ee7e60b93c9527b3699e86afdb5
KozulevDV <den1119997@yandex.ru>
2024-09-22 01:32:38 +0300

    Выполнены задания: 1) Ввести диапазон, в котором игрок угадывает. Например, от 1 до 100. 2) Рассказывать игроку, его предположение больше или меньше искомого числа. 3) Обновлять диапазоны после каждого предположения. * 4) Вычислять максимальное количество попыток автоматически (капелька математики). ** 5) Сделать бота, который, наоборот, отгадывает число игрока.

--- Net18Online/Net18Online/Models/GuessTheNumber.cs
+++ Net18Online/Net18Online/Models/GuessTheNumber.cs
@@ -14,16 +14,58 @@ namespace Net18Online.Models
         /// Max attempt before lose
         /// </summary>
         public int MaxAttempt { get; set; }
+        /// <summary>
+        /// Min number value
+        /// </summary>
+        public int MinNumber { get; set; }
+        /// <summary>
+        /// Max number value
+        /// </summary>
+        public int MaxNumber { get; set; }
+        /// <summary>
+        /// Minimum number to prompt for user input
+        /// </summary>
+        private int _minGuessNumber = 0;
+        /// <summary>
+        /// Maximum number to prompt for user input
+        /// </summary>
+        private int _maxGuessNumber = 0;
 
         private bool _isSecondGamerWin;
 
         public void Start()
         {
-            FirstGamerSetTheNumber();
+            Console.WriteLine("Select game mode:");
+            Console.WriteLine("1 - Two players. The first one makes a guess. The second one guesses.");
+            Console.WriteLine("2 - Bot guesses the player's number");
+            Console.WriteLine("0 - Exit");
 
-            SecondGamerTryGuessTheNumber();
+            int gameMode;
+            do
+            {
+                gameMode = ReadNumber($"Enter game mode:");
+            } while (!Enumerable.Range(0, 3).Contains(gameMode));
 
-            ShowResult();
+            Console.Clear();
+
+            switch (gameMode)
+            {
+                case 1:
+                    {
+                        FirstGamerSetTheNumber();
+                        SecondGamerTryGuessTheNumber();
+                        ShowResult();
+                    }
+                    break;
+                case 2:
+                    {
+                        SetTheNumber();
+                        BotGuessesTheNumber();
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void ShowResult()
@@ -41,13 +83,18 @@ namespace Net18Online.Models
         private void SecondGamerTryGuessTheNumber()
         {
             var attempt = 0;
+            var guess = 0;
             do
             {
-                var guess = ReadNumber("Enter your guess");
+                guess = ReadNumber($"{GetHelper(attempt, guess)} {GetGuessCalcCount()}\nEnter your guess in the range from {_minGuessNumber} to {_maxGuessNumber} (attemt {attempt+1} of {MaxAttempt}):");
                 if (guess == Number)
                 {
                     _isSecondGamerWin = true;
                     break;
+                }
+                else
+                {
+                    UpdateGuessRange(guess);
                 }
                 attempt++;
             } while (attempt < MaxAttempt);
@@ -55,16 +102,94 @@ namespace Net18Online.Models
 
         private void FirstGamerSetTheNumber()
         {
-            Number = ReadNumber("Enter the number");
+            SetTheNumber();
+
             MaxAttempt = ReadNumber("Enter max attempt count");
             Console.Clear();
         }
 
+        private void SetTheNumber()
+        {
+            MinNumber = ReadNumber("Enter min value number");
+            _minGuessNumber = MinNumber;
+
+            do
+            {
+                MaxNumber = ReadNumber($"Enter max value number");
+                if (MaxNumber >= MinNumber)
+                {
+                    _maxGuessNumber = MaxNumber;
+                    break;
+                }
+            } while (true);
+
+            do
+            {
+                Number = ReadNumber($"Enter the number in range from {MinNumber} to {MaxNumber}");
+                if (MinNumber <= Number && Number <= MaxNumber)
+                {
+                    break;
+                }
+            } while (true);
+        }
+
         private int ReadNumber(string message)
         {
-            Console.WriteLine(message);
-            var numberStr = Console.ReadLine();
-            return int.Parse(numberStr);
+            do
+            { 
+                Console.WriteLine(message);
+                var numberStr = Console.ReadLine();
+
+                if (int.TryParse(numberStr, out int value))
+                {
+                    return value;
+                }
+            } while (true);
+        }
+
+        private void UpdateGuessRange(int guessNumber)
+        {
+            if (Enumerable.Range(_minGuessNumber, Number - _minGuessNumber + 1).Contains(guessNumber))
+            {
+                _minGuessNumber = guessNumber + 1;
+            }
+            else if(Enumerable.Range(Number, _maxGuessNumber - Number + 1).Contains(guessNumber))
+            {
+                _maxGuessNumber = guessNumber - 1;
+            }
+        }
+
+        private string GetHelper(int attemptNum, int prevGuess)
+        {
+            return attemptNum == 0 ? string.Empty :
+                prevGuess < Number ? "The guessed number is higher." : "The guessed number is less.";
+        }
+
+        private string GetGuessCalcCount()
+        {
+            return $"It is possible to guess in {Math.Ceiling(Math.Log2(_maxGuessNumber - _minGuessNumber + 1))} attempts.";
+        }
+
+        private void BotGuessesTheNumber()
+        {
+            var attempt = 0;
+            var guess = 0;
+            do
+            {
+                attempt++;
+
+                guess = GetBotNumber();
+                Console.WriteLine($"Attempt #{attempt}: {guess}");
+                
+                UpdateGuessRange(guess);
+            } while (guess != Number);
+
+            Console.WriteLine($"Number guessed. Number of attempts - {attempt}.");
+        }
+
+        private int GetBotNumber()
+        {
+            return _minGuessNumber + (int)Math.Ceiling((double)(_maxGuessNumber - _minGuessNumber) / 2);
         }
     }
 }
