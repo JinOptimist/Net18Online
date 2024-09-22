267ea867440a9ac77dc67ec5e3e65de16493e25f
Gleb <flareon.game@gmail.com>
2024-09-23 00:33:58 +0300

    Расширить игру Угадай число 1) Ввести диапазон в котором игрок угадывает. Например от 1 до 100 2) Рассказывать игроку, его предположение больше или меньше искомого числа 3) Обновлять диапазоны после каждого предположения * 4) Вычислять максимальное количество попыток автоматически (капелька математики) ** 5) Сделать бота который наоборот отгадывает число игрока

--- Net18Online/Net18Online/Models/GuessTheNumber.cs
+++ Net18Online/Net18Online/Models/GuessTheNumber.cs
@@ -1,9 +1,10 @@
 ﻿
-
+using System.Reflection.Metadata.Ecma335;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Net18Online.Models
 {
-    public class GuessTheNumber
+    public class GuessTheNumber // Game with 2 gamers
     {
         /// <summary>
         /// Number which we try to guess
@@ -11,14 +12,32 @@ namespace Net18Online.Models
         public int Number { get; set; }
 
         /// <summary>
+        /// Range start number
+        /// </summary>
+        public int Num_Start_Range { get; set; }
+
+        /// <summary>
+        /// Range end number
+        /// </summary>
+        public int Num_End_Range { get; set; }
+
+        /// <summary>
         /// Max attempt before lose
         /// </summary>
         public int MaxAttempt { get; set; }
 
+        /// <summary>
+        /// Is the second player the winner
+        /// </summary>
         private bool _isSecondGamerWin;
 
+        /// <summary>
+        /// Start Game
+        /// </summary>
         public void Start()
         {
+            SelectRange();
+
             FirstGamerSetTheNumber();
 
             SecondGamerTryGuessTheNumber();
@@ -26,45 +45,364 @@ namespace Net18Online.Models
             ShowResult();
         }
 
-        private void ShowResult()
+        private void SelectRange() // Selecting the limits of the range and checking the condition for the existence of the range
         {
-            if (_isSecondGamerWin)
+            Console.Clear();
+            Console.WriteLine("First you need to select a range of numbers");
+            Num_Start_Range = ReadNumber("Enter a number - the beginning of the range");
+            Console.WriteLine("\nCool, now let's enter the end of the range");
+            Num_End_Range = ReadNumber("Enter a number - the ending of the range");
+
+            while (true) // Condition for the existence of a range
             {
-                Console.WriteLine("Congratz");
-            }
-            else
-            {
-                Console.WriteLine("Looser");
+                if (Num_End_Range < Num_Start_Range || Num_End_Range == Num_Start_Range)
+                {
+                    Num_End_Range = 0;
+                    Num_End_Range = ReadNumber("\nOops, the end of the range is less than the beginning. " +
+                        "Please enter a number greater than " + Num_Start_Range);
+                }
+                else { break; }
             }
         }
 
-        private void SecondGamerTryGuessTheNumber()
+        static int CalculateMaxAttempts(int num_start_range, int num_end_range) // We calculate the maximum number of attempts using the formula
         {
-            var attempt = 0;
-            do
+            int range = num_end_range - num_start_range + 1;
+            return (int)Math.Ceiling(Math.Log2(range));
+        }
+
+        private void FirstGamerSetTheNumber() // The first player chooses a number in the range and the maximum number of attempts
+        {
+            Console.Clear();
+            Number = ReadNumber("Enter the number in range " + Num_Start_Range + " : " + Num_End_Range);
+            if (Number < Num_Start_Range || Number > Num_End_Range) // Checking for a number in a range
             {
-                var guess = ReadNumber("Enter your guess");
-                if (guess == Number)
+                Console.WriteLine("Your number is out of range. Press any button");
+                Console.ReadLine();
+                FirstGamerSetTheNumber();
+            }
+            MaxAttempt = CalculateMaxAttempts(Num_Start_Range, Num_End_Range); // Calculate the maximum number of attempts
+            Console.WriteLine("\nMaximum number of attempts for this range is " + MaxAttempt);
+            while (true) // We ask the player if he wants to change the number of attempts
+            {
+                Console.WriteLine("\nEnter \"Y\" to save the number of attempts or \"N\" to change");
+                Console.WriteLine("Your answer...");
+                var choose = Console.ReadLine();
+                if (choose == "Y")
+                {
+                    break;
+                }
+                else if (choose == "N")
+                {
+                    MaxAttempt = ReadNumber("\nEnter the maximum number of attempts");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nCould not recognize the answer, try again.");
+                }
+            }
+            Console.Clear();
+            
+        }
+
+        private void SecondGamerTryGuessTheNumber() // The second player tries to guess the number knowing the range and receiving hints
+        {
+            
+            var attempt = 0;
+            while(attempt < MaxAttempt) // While the number of attempts is less than the maximum number of attempts
+            {
+                attempt++;
+                var guess = ReadNumber("You need to guess a number in a range "
+                    + Num_Start_Range + " : " + Num_End_Range + "\nEnter your guess");
+                if (guess == Number) // The second player guessed right
                 {
                     _isSecondGamerWin = true;
                     break;
                 }
-                attempt++;
-            } while (attempt < MaxAttempt);
+                else if (guess < Number) // Number is less than the hidden number
+                {
+                    Console.WriteLine("\nOh no, your number is less than the hidden number. " +
+                        "You have " + (MaxAttempt - attempt) + " more try");
+                    Num_Start_Range = guess;
+                }
+                else if (guess > Number) // Number is greater than the hidden number
+                {
+                    Console.WriteLine("\nOh no, your number is greater than the hidden number. " +
+                        "You have " + (MaxAttempt - attempt) + " more try");
+                    Num_End_Range = guess;
+                }
+            }
+        }      
+
+        private int ReadNumber(string message) // We read the string, check if it can be a number and convert it to int
+        {
+            while (true) // We are trying to get numbers from the player
+            {
+                Console.WriteLine(message);
+                var numberStr = Console.ReadLine();
+                if (int.TryParse(numberStr, out int number)) // Checking if it is a number
+                {
+                    return int.Parse(numberStr); // Transform
+                }
+                else
+                {
+                    Console.WriteLine("Could not recognize the number, try again."); // Try again
+                }
+            }
         }
 
-        private void FirstGamerSetTheNumber()
+        private void ShowResult() // Let's find out the results
         {
-            Number = ReadNumber("Enter the number");
-            MaxAttempt = ReadNumber("Enter max attempt count");
+            if (_isSecondGamerWin)
+            {
+                Console.WriteLine("\nCongratz");
+            }
+            else
+            {
+                Console.WriteLine("\nLooser");
+            }
+        }
+
+    }
+
+    public class GuessTheNumberWithComputer() // Game with computer
+    {
+        /// <summary>
+        /// Number which we try to guess
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Range start number
+        /// </summary>
+        public int Num_Start_Range { get; set; }
+
+        /// <summary>
+        /// Range end number
+        /// </summary>
+        public int Num_End_Range { get; set; }
+
+        /// <summary>
+        /// Max attempt before lose
+        /// </summary>
+        public int MaxAttempt { get; set; }
+
+        /// <summary>
+        /// Is the computer the winner
+        /// </summary>
+        private bool _isComputerWin;
+
+        /// <summary>
+        /// Start Game
+        /// </summary>
+        /// 
+        public void Start()
+        {
+            SelectRange();
+
+            GamerSetTheNumber();
+
+            TypeOfSearch();
+
+
+        }
+
+        private void SelectRange() // Selecting the limits of the range and checking the condition for the existence of the range
+        {
             Console.Clear();
+            Console.WriteLine("First you need to select a range of numbers");
+            Num_Start_Range = ReadNumber("Enter a number - the beginning of the range");
+            Console.WriteLine("\nCool, now let's enter the end of the range");
+            Num_End_Range = ReadNumber("Enter a number - the ending of the range");
+
+            while (true) // Condition for the existence of a range
+            {
+                if (Num_End_Range < Num_Start_Range || Num_End_Range == Num_Start_Range)
+                {
+                    Num_End_Range = 0;
+                    Num_End_Range = ReadNumber("\nOops, the end of the range is less than the beginning. " +
+                        "Please enter a number greater than " + Num_Start_Range);
+                }
+                else { break; }
+            }
         }
 
-        private int ReadNumber(string message)
+        private void GamerSetTheNumber() // The first player chooses a number in the range
         {
-            Console.WriteLine(message);
-            var numberStr = Console.ReadLine();
-            return int.Parse(numberStr);
+            Console.Clear();
+            Number = ReadNumber("Enter the number in range " + Num_Start_Range + " : " + Num_End_Range);
+            if (Number < Num_Start_Range || Number > Num_End_Range) // Checking for a number in a range
+            {
+                Console.WriteLine("Your number is out of range. Press any button");
+                Console.ReadLine();
+                GamerSetTheNumber();
+            }
+            Console.Clear();
+
         }
+
+        private void TypeOfSearch()
+        {
+            Console.Clear();
+            Console.WriteLine("Now you need to choose a method for searching for a number." +
+                    " \nThere are 2 options to choose from: " +
+                    "\n1 - You give the computer several attempts, and it tries to guess your number;" +
+                    "\n2 - You give the computer an infinite number of tries and see how long it takes to guess the number");  
+            while (true)
+            {
+                Console.WriteLine("\r\nEnter 1 to select the first option or enter 2 to select the second option");
+                Console.WriteLine("Your answer...");
+                var answer = Console.ReadLine();
+                if (answer == "1")
+                {
+                    ComputerTryGuessTheNumberWithAttepmts();
+                    break;
+                }
+                else if (answer == "2")
+                {
+                    ComputerTryGuessTheNumberWithoutAttempts();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nCould not recognize the answer, try again.");
+
+                }
+            }
+        }
+        
+
+        private void ComputerTryGuessTheNumberWithAttepmts()
+        {
+            Console.Clear();
+            MaxAttempt = CalculateMaxAttempts(Num_Start_Range, Num_End_Range); // Calculate the maximum number of attempts
+            Console.WriteLine("\nMaximum number of attempts for this range is " + MaxAttempt);
+            while (true) // We ask the player if he wants to change the number of attempts
+            {
+                Console.WriteLine("\nEnter \"Y\" to save the number of attempts or \"N\" to change");
+                Console.WriteLine("Your answer...");
+                var choose = Console.ReadLine();
+                if (choose == "Y")
+                {
+                    break;
+                }
+                else if (choose == "N")
+                {
+                    MaxAttempt = ReadNumber("\nEnter the maximum number of attempts");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nCould not recognize the answer, try again.");
+                }
+            }
+
+            var attempt = 0;
+            while (attempt < MaxAttempt) // While the number of attempts is less than the maximum number of attempts
+            {
+                attempt++;
+                Console.Clear();
+                Console.WriteLine("Number of attempts: " + attempt);
+                Random rnd = new Random();
+                int guess = rnd.Next(Num_Start_Range, Num_End_Range);
+                Console.WriteLine("\nI think your number is " + guess);
+                Console.WriteLine("\nYou must give a hint to the computer. " +
+                    "\nEnter \"H\" if your number is higher;\n \"L\" if lower;\n \"Y\" if I guessed right");
+                Console.WriteLine("Your answer...");
+                var answer = Console.ReadLine();
+                if (answer == "H") // Number is greater than the hidden number
+                {
+                    Num_Start_Range = guess + 1;
+                }
+                else if (answer == "L") // Number is less than the hidden number
+                {
+                    Num_End_Range = guess - 1;
+                }
+                else if (answer == "Y") // Computer won
+                {
+                    Console.WriteLine("\nI always win :)");
+                    Console.WriteLine("Attempts used: " + attempt);
+                    _isComputerWin = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease, answer \"H\", \"L\" or \"Y\" ");
+                    attempt--;
+                    Console.WriteLine("Press any button");
+                    Console.ReadLine();
+
+                }
+            }
+            if (_isComputerWin == false)
+            {
+                Console.WriteLine("\nHow did I lose?");
+                
+            }
+            
+        }
+        private void ComputerTryGuessTheNumberWithoutAttempts() // The computer tries to guess the number knowing the range and receiving hints
+        {
+            var attempts = 0;
+            while (true) // Until the computer guesses the number
+            {
+                attempts++;
+                Console.Clear();
+                Console.WriteLine("Number of attempts: " + attempts);
+                var guess = (Num_Start_Range + Num_End_Range) / 2; // binary search
+                Console.WriteLine("\nI think your number is " + guess);
+                Console.WriteLine("\nYou must give a hint to the computer. " +
+                    "\nEnter \"H\" if your number is higher;\n \"L\" if lower;\n \"Y\" if I guessed right");
+                Console.WriteLine("Your answer...");
+                var answer = Console.ReadLine();
+                if (answer == "H") // Number is greater than the hidden number
+                {
+                    Num_Start_Range = guess + 1;
+                }
+                else if (answer == "L") // Number is less than the hidden number
+                {
+                    Num_End_Range = guess - 1;
+                }
+                else if (answer == "Y") // Computer won
+                {
+                    Console.WriteLine("\nI always win :)");
+                    Console.WriteLine("Attempts used: " + attempts);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease, answer \"H\", \"L\" or \"Y\" ");
+                    attempts--;
+                    Console.WriteLine("Press any button");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static int CalculateMaxAttempts(int num_start_range, int num_end_range) // We calculate the maximum number of attempts using the formula
+        {
+            int range = num_end_range - num_start_range + 1;
+            return (int)Math.Ceiling(Math.Log2(range));
+        }
+
+
+        private int ReadNumber(string message) // We read the string, check if it can be a number and convert it to int
+        {
+            while (true) // We are trying to get numbers from the player
+            {
+                Console.WriteLine(message);
+                var numberStr = Console.ReadLine();
+                if (int.TryParse(numberStr, out int number)) // Checking if it is a number
+                {
+                    return int.Parse(numberStr); // Transform
+                }
+                else
+                {
+                    Console.WriteLine("Could not recognize the number, try again."); // Try again
+                }
+            }
+        }
+
     }
 }
--- Net18Online/Net18Online/Program.cs
+++ Net18Online/Net18Online/Program.cs
@@ -1,4 +1,27 @@
 ﻿using Net18Online.Models;
 
-var game1 = new GuessTheNumber();
-game1.Start();
+Console.WriteLine("Hello, let's play? " +
+    "\nThere are two games to choose from: " +
+    "\nGame 1 for two players (the first guesses, the second guesses), " +
+    "\nGame 2 for single people :( (You guess the number, and the computer guesses it)");
+while (true)
+{
+    Console.WriteLine("\nEnter \"G1\" to play the Game1 or \"G2\" to play the Game2");
+    var choose_game = Console.ReadLine();
+    if (choose_game == "G1")
+    {
+        var game1 = new GuessTheNumber();
+        game1.Start();
+        break;
+    }
+    else if (choose_game == "G2")
+    {
+        var game2 = new GuessTheNumberWithComputer();
+        game2.Start();
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nCould not recognize the answer, try again.");
+    }
+}
