ee5716727a99f43259e378be8822d5b36af5d357
Vezd <vezd228@gmail.com>
2024-09-23 16:46:41 +0300

    Update GuessTheNumber.cs

--- Net18Online/Net18Online/Models/GuessTheNumber.cs
+++ Net18Online/Net18Online/Models/GuessTheNumber.cs
@@ -1,6 +1,8 @@
 ﻿
 
 
+using System;
+
 namespace Net18Online.Models
 {
     public class GuessTheNumber
@@ -15,18 +17,69 @@ namespace Net18Online.Models
         /// </summary>
         public int MaxAttempt { get; set; }
 
+        public int Range { get; set; }
+
         private bool _isSecondGamerWin;
+
+        private bool _isBotWin;
 
         public void Start()
         {
+            int temp = 0;
+            do
+            {
+                int selecter;
+                selecter = ReadNumber("1 - play with human, 2 - play with bot");
+                if (selecter == 1)
+                {
+                    PlayWithHuman();
+                    temp++;
+                }
+                else if (selecter == 2)
+                {
+                    PlayWithBot();
+                    temp++;
+                }
+                else
+                {
+                    Console.WriteLine("You need to write 1 or 2");
+                }
+            } while (temp < 1);
+        }
+
+        public void PlayWithBot()
+        {
+
+            FirstGamerSetTheNumber();
+
+            BotTryGuessTheNumber();
+
+            ShowResultWithBot();
+        }
+
+        public void PlayWithHuman()
+        {
+
             FirstGamerSetTheNumber();
 
             SecondGamerTryGuessTheNumber();
 
-            ShowResult();
+            ShowResultWithHuman();
         }
 
-        private void ShowResult()
+        private void ShowResultWithBot()
+        {
+            if (_isBotWin)
+            {
+                Console.WriteLine("Bot win");
+            }
+            else
+            {
+                Console.WriteLine("Human win");
+            }
+        }
+
+        private void ShowResultWithHuman()
         {
             if (_isSecondGamerWin)
             {
@@ -43,19 +96,102 @@ namespace Net18Online.Models
             var attempt = 0;
             do
             {
+                Random rnd = new Random();
                 var guess = ReadNumber("Enter your guess");
-                if (guess == Number)
+                if (guess < 1 | guess > Range)
                 {
-                    _isSecondGamerWin = true;
-                    break;
+                    Console.WriteLine($"Write number for 1 to {Range}");
                 }
-                attempt++;
+                else
+                {
+                    if (guess == Number)
+                    {
+                        _isSecondGamerWin = true;
+                        break;
+                    }
+                    else if (guess > Number)
+                    {
+                        Console.WriteLine("You need a smaller number");
+                        Range = rnd.Next(Range, Range+50);
+                    }
+                    else if (guess < Number)
+                    {
+                        Console.WriteLine("You need a higher number");
+                        Range = rnd.Next(Range, Range+50);
+                    }
+                    attempt++;
+                }
             } while (attempt < MaxAttempt);
         }
 
+        private void BotTryGuessTheNumber()
+        {
+            int botMin = 0;
+            int botMax = Range+50;
+            var attempt = 0;
+            do
+            {
+                System.Threading.Thread.Sleep(200);
+                Random rndRange = new Random();
+                Random rndNumner = new Random();
+                int guess = rndNumner.Next(botMin, botMax);
+                if (guess < 1 | guess > Range)
+                {
+                    Console.WriteLine($"Bot write illegal number {guess}");
+                }
+                else
+                {
+                    if (guess == Number)
+                    {
+                        Console.WriteLine($"Bot write number {guess}");
+                        _isBotWin = true;
+                        break;
+                    }
+                    else if (guess > Number)
+                    {
+                        Console.WriteLine($"Bot write number {guess}");
+                        botMax = guess-1;
+                        Range = rndRange.Next(Range, Range+50);
+                    }
+                    else if (guess < Number)
+                    {
+                        Console.WriteLine($"Bot write number {guess}");
+                        botMin = guess+1;
+                        Range = rndRange.Next(Range, Range+50);
+                    }
+                    attempt++;
+                }
+            } while (attempt < MaxAttempt);
+        }
         private void FirstGamerSetTheNumber()
         {
-            Number = ReadNumber("Enter the number");
+            int temp = 0;
+            int temp2 = 0;
+            do
+            {
+                Range = ReadNumber("Enter the range"); ;
+                if (Range > 0 & Range <= 500)
+                {
+                    temp2++;
+                }
+                else
+                {
+                    Console.WriteLine("You need to set range from 1 to 500");
+                }
+            } while (temp2 < 1);
+
+            do
+            {
+                Number = ReadNumber("Enter the number");
+                if (Number > 0 & Number <= Range)
+                {
+                    temp++;
+                }
+                else
+                {
+                    Console.WriteLine("You need to set number from 1 to 100");
+                }
+            } while (temp < 1);
             MaxAttempt = ReadNumber("Enter max attempt count");
             Console.Clear();
         }
