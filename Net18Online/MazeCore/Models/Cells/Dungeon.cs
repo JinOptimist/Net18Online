27d04fa4b748100813cc7b3921ec3fccf794a73e
glytop <123397208+glytop@users.noreply.github.com>
2024-10-01 00:00:45 +0300

    Generation of Dungeon and fix problems

--- Net18Online/MazeCore/Models/Cells/Dungeon.cs
+++ Net18Online/MazeCore/Models/Cells/Dungeon.cs
@@ -1,5 +1,8 @@
-﻿using MazeCore.Models;
+﻿using MazeCore.Helpers;
+using MazeCore.Models;
 using MazeCore.Models.Cells.Character;
+using MazeCore.Models.Cells.Enums;
+using System;
 using System.Text;
 
 namespace MazeCore.Models.Cells
@@ -15,12 +18,79 @@ namespace MazeCore.Models.Cells
 
         public override void InteractWithCell(BaseCharacter character)
         {
-            AddEventInfo("Step Step Step....");
+            Maze[X, Y] = new Ground(X, Y, Maze);
+            var maxValue = GetEnumMaxValue<Treasure>();
+            var treasureValue = Maze.Random.Next(1, maxValue + 1);
+            var treasure = GetTreasureByValue(treasureValue);
+
+            AddEventInfo($"You've discovered {treasure} in the dungeon");
+
+            switch (treasure)
+            {
+                case Treasure.GodOfBlood:
+                    var monsterTreasure = GetMonsterValue(character);
+                    AddEventInfo($"The God of blood changed your health level to {character.Health}, but he gave you {monsterTreasure} in return ");
+                    break;
+                case Treasure.HealthPoints:
+                    var procent = (double)character.Health / 100 * 20;
+                    character.Health += (int)procent;
+                    AddEventInfo($"You found some health points, your health has increased by 20%. Now you have {character.Health}");
+                    break;
+            }
+
+            Console.ReadKey();
+
         }
 
         public override bool TryStep(BaseCharacter character)
         {
+            AddEventInfo("Are you sure you want to go down to the dungeon?");
             return true;
         }
+
+
+        private Treasure GetTreasureByValue(int treasureValue)
+        {
+            if (treasureValue >= 1 && treasureValue <= 2)
+            {
+                return Treasure.Noting;
+            }
+            if (treasureValue >= 3 && treasureValue <= 6)
+            {
+                return Treasure.GodOfBlood;
+            }
+            if (treasureValue == 7)
+            {
+                return Treasure.HealthPoints;
+            }
+            if (treasureValue == 8)
+            {
+                return Treasure.HandfulOfCoins;
+            }
+            else
+            {
+                return Treasure.LotOfCoins;
+            }
+        }
+
+        private MonsterTreasure GetMonsterValue(BaseCharacter character)
+        {
+            var procent = (double)character.Health / 100 * Maze.Random.Next(0, 51);
+            character.Health -= (int)procent;
+            var maxValue = GetEnumMaxValue<MonsterTreasure>();
+            var treasureValue = Maze.Random.Next(1, maxValue + 1);
+
+            if (treasureValue >= 1 && treasureValue <= 3)
+            {
+                return MonsterTreasure.Noting;
+            }
+
+            return (MonsterTreasure)treasureValue;
+        }
+        public static int GetEnumMaxValue<T>() where T : Enum => Enum
+                .GetValues(typeof(T))
+                .Cast<int>()
+                .Max();
     }
 }
+
---
+++ Net18Online/MazeCore/Models/Cells/Enums/DungeonTreasure.cs
@@ -0,0 +1,11 @@
+﻿namespace MazeCore.Models.Cells.Enums
+{
+    public enum Treasure
+    {
+        Noting = 1,
+        GodOfBlood = 3,
+        HealthPoints = 7,
+        HandfulOfCoins = 8,
+        LotOfCoins = 9,
+    }
+}
