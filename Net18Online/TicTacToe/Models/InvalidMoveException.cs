cb57913ea2be0072e6b2d294b3405d6ab7fbb249
July-cloud <inelipovich99@gmail.com>
2024-10-09 13:40:25 +0300

    Create game TicTacToe

---
+++ Net18Online/TicTacToe/Models/InvalidMoveException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class InvalidMoveException : Exception
+    {
+        public InvalidMoveException()
+        {
+        }
+
+        public InvalidMoveException(string message) : base(message)
+        {
+        }
+
+        public InvalidMoveException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected InvalidMoveException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
