d3f53455368faeeaa2f9f6f67250f0bd389325e3
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-14 21:46:38 +0300

    I did the tests without watching the lesson to the end it turns out I need to go to the wrong classes

--- Net18Online/MazeCoreTest/Models/Cells/MagicTest.cs
+++ Net18Online/MazeCoreTest/Models/Cells/MagicTest.cs
@@ -16,8 +16,10 @@ namespace MazeCoreTest.Models.Cells
     {
         private Mock<IMaze> _mazeMock;
         private Mock<IBaseCharacter> _characterMock;
+        private Mock<IBaseCell> _ground;
 
         private Magic _magic;
+
 
 
         // @BeforEach
@@ -27,8 +29,12 @@ namespace MazeCoreTest.Models.Cells
             // Fake
             _mazeMock = new Mock<IMaze>();
             _characterMock = new Mock<IBaseCharacter>();
+            _ground = new Mock<IBaseCell>();
+
             //Real
             _magic = new Magic(3, 3, _mazeMock.Object);
+
+
         }
         //@Test
         [Test]
@@ -44,9 +50,10 @@ namespace MazeCoreTest.Models.Cells
 
             Assert.That(actual1, Is.True, "You can`t step this"); // Message up , if test fails
         }
+
         [Test]
         [TestCase(0, 1)]
-        [TestCase(12, 13)]
+        [TestCase(12, 13)]  // Сколько магии было и стало
         [TestCase(26, 27)]
         [TestCase(111, 112)]
         public void TryStep_CheckThatWhereHaracterStepThanHeGetAMagic
@@ -59,6 +66,33 @@ namespace MazeCoreTest.Models.Cells
             Assert.That(_characterMock.Object.Magic,
                 Is.EqualTo(magicOnFinish),
                 "Where our _many_ magic ? Billy ?"); // Message up , if test fails
+        }
+
+        [Test]
+        [TestCase(0)]  // Начальная магия
+        [TestCase(10)]
+        [TestCase(50)]
+        public void TryStep_CheckThatMagicDoesNotChangeIfNotMagicCell(int initialMagic)
+        {
+            _characterMock.SetupProperty(x => x.Magic);  // Устанавливаем начальную магию у персонажа
+            _characterMock.Object.Magic = initialMagic;
+
+            _ground.CallBase = true;
+
+            _ground.Object.TryStep(_characterMock.Object);
+
+            Assert.That(_characterMock.Object.Magic,
+                Is.EqualTo(initialMagic),
+                "Magic should not change when stepping on a non-magic cell.");
+        }
+
+
+
+        [Test]
+        public void TryStep_CheckThatMagicWasReplacedToGround()  // Тест проверяет, что метод был вызван
+        {
+            _magic.TryStep(_characterMock.Object);
+            _mazeMock.Verify(x => x.ReplaceCellToGround(_magic), Times.Once);
         }
     }
 }
