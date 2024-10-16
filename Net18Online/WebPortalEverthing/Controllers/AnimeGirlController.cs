dcdcd01ea25bd1a11260bb6629bbac4fa90c5486
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-10-16 19:52:09 +0300

    Add web app

---
+++ Net18Online/WebPortalEverthing/Controllers/AnimeGirlController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortalEverthing.Controllers
+{
+    public class AnimeGirlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AllGirls()
+        {
+            return View();
+        }
+    }
+}
---
+++ Net18Online/WebPortalEverthing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebPortalEverthing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
