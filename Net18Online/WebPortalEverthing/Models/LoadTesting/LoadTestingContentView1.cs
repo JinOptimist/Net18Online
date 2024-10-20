045c0af6873c951bac469a511f4ded8b1be74e7c
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-20 18:38:59 +0300

    public class LoadTestingContentView1  {      public Guid GUID { get; set; }      public string Name { get; set; }      public decimal Throughput { get; set; }      public decimal Average { get; set; }  }

---
+++ Net18Online/WebPortalEverthing/Models/LoadTesting/LoadTestingContentView1.cs
@@ -0,0 +1,10 @@
+﻿namespace WebPortalEverthing.Models.LoadTesting
+{
+    public class LoadTestingContentView1
+    {
+        public Guid GUID { get; set; }
+        public string Name { get; set; }
+        public decimal Throughput { get; set; }
+        public decimal Average { get; set; }
+    }
+}
