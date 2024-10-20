0957fbbb748894b0499c1e8001a670d2cdd936f5
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-20 20:07:10 +0300

    входные параметры приходят снаружи от пользователя страницы в первую метрику

--- Net18Online/WebPortalEverthing/Controllers/LoadTesting/LoadTestingController.cs
+++ Net18Online/WebPortalEverthing/Controllers/LoadTesting/LoadTestingController.cs
@@ -5,13 +5,17 @@ namespace WebPortalEverthing.Controllers.LoadTesting
 {
     public class LoadTestingController : Controller
     {
-        public IActionResult contentView1()
+        public IActionResult contentView1(
+            decimal Throughput,
+            decimal Average)//входные параметры приходят снаружи от пользователя страницы
         {
             // Инициализация модели
             var model = new LoadTestingContentViewModel1();
+            model.Metrics[0].Throughput = Throughput;
+            model.Metrics[0].Average = Average;
 
             // Передаем модель в представление
-            return View(model);
+            return View(model); //model выдаст данные наружу, на страницу
         }
     }
 }
