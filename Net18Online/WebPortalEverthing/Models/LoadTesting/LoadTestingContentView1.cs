4785c8ba4d0d5f93fe898e756c2f70c2f12a6f48
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-20 19:04:20 +0300

    add List<Metric> Metrics

--- Net18Online/WebPortalEverthing/Models/LoadTesting/LoadTestingContentView1.cs
+++ Net18Online/WebPortalEverthing/Models/LoadTesting/LoadTestingContentView1.cs
@@ -3,6 +3,23 @@
     public class LoadTestingContentView1
     {
         public List<Metric> Metrics { get; set; }
-        
+
+        // Конструктор класса, инициализирующий список метрик
+        public LoadTestingContentView1()
+        {
+            Metrics = new List<Metric>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Metrics.Add(new Metric
+                {
+                    GUID = Guid.NewGuid(),
+                    Name = $"Metric {i}",
+                    Throughput = i * 10.5m, // Пример значений, изменяемых в зависимости от итерации
+                    Average = i * 5.0m
+                });
+            }
+        }
+
     }
 }
