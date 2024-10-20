7e705da457a0da9bf764c8c0289593612aa512ee
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-20 13:11:19 +0300

    Наверстать 1 HTML страницу с картинками и менюшкой +css +img

---
+++ Net18Online/WebPortalEverthing/Controllers/LoadTesting/LoadTesting.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortalEverthing.Controllers.LoadTesting
+{
+    public class LoadTesting : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
