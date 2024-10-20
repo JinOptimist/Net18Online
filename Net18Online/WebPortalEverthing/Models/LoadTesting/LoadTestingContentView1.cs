37c3a9bd69a47e289f24b7e9d8c2757d3fcc3870
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-20 18:42:06 +0300

    add model Metric

--- Net18Online/WebPortalEverthing/Models/LoadTesting/LoadTestingContentView1.cs
+++ Net18Online/WebPortalEverthing/Models/LoadTesting/LoadTestingContentView1.cs
@@ -2,9 +2,7 @@
 {
     public class LoadTestingContentView1
     {
-        public Guid GUID { get; set; }
-        public string Name { get; set; }
-        public decimal Throughput { get; set; }
-        public decimal Average { get; set; }
+        public List<Metric> Metrics { get; set; }
+        
     }
 }
