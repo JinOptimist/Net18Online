ffc1032b33ab9b8221c8218c9bf786139032839a
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-21 15:42:07 +0300

    _LayoutLoadTestingModel add FridaysRemaining

---
+++ Net18Online/WebPortalEverthing/Controllers/LoadTesting/LayoutLoadTestingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPortalEverthing.Models.LoadTesting;
+
+namespace WebPortalEverthing.Controllers.LoadTesting
+{
+    public class LayoutLoadTestingController : Controller
+    {
+        public IActionResult _LayoutLoadTesting()
+        {
+            // Инициализация модели
+            var model = new _LayoutLoadTestingModel();
+
+            // Передаем модель в представление
+            return View(model); //model выдаст данные наружу, на страницу
+
+        }
+    }
+}
---
+++ Net18Online/WebPortalEverthing/Models/LoadTesting/_LayoutLoadTestingModel.cs
@@ -0,0 +1,30 @@
+﻿namespace WebPortalEverthing.Models.LoadTesting
+{
+    public class _LayoutLoadTestingModel
+    {
+        public int FridaysRemaining { get; set; }
+
+        public _LayoutLoadTestingModel()
+        {
+            // Текущая дата
+            DateTime today = DateTime.Now;
+
+            // Дата наступления Нового Года
+            DateTime newYear = new DateTime(today.Year + 1, 1, 1);
+
+            // Количество пятниц до Нового Года
+            FridaysRemaining = 0;
+
+            // Проход по дням до Нового Года
+            DateTime currentDay = today;
+            while (currentDay < newYear)
+            {
+                if (currentDay.DayOfWeek == DayOfWeek.Friday)
+                {
+                    FridaysRemaining++;
+                }
+                currentDay = currentDay.AddDays(1);
+            }
+        }
+    }
+}
