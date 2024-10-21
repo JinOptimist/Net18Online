dbadcbfe7335ac820a32e926598bb32c4feeaf7d
Gleb <flareon.game@gmail.com>
2024-10-21 17:58:10 +0300

    Added movies site

---
+++ Net18Online/WebPortalEverthing/Controllers/MoviePosterController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPortalEverthing.Models;
+using WebPortalEverthing.Models.MoviePoster;
+
+namespace WebPortalEverthing.Controllers
+{
+    public class MoviePosterController : Controller
+    {
+        // BAD. DO NOT USE THIS ON PROD
+        public static List<PosterViewModel> posterViewModels = new List<PosterViewModel>();
+        public static List<PosterViewModel> newPosterInListOfPostersViewModels = new List<PosterViewModel>();
+
+        public IActionResult Index(string name, int age)
+        {
+            var model = new MoviePosterIndexViewModel();
+            model.Hours = DateTime.Now.Hour;
+            model.Minutes = DateTime.Now.Minute;
+            model.Seconds = DateTime.Now.Second;
+            return View(model);
+        }
+
+        public IActionResult AllPosters(int? count)
+        {
+            posterViewModels.Clear();
+            if (!posterViewModels.Any())
+            {
+                for (int i = 0; i < (count ?? 4); i++)
+                {
+                    var posterNumber = (i % 4) + 1;
+                    var viewModel = new PosterViewModel
+                    {
+                        Name = $"Poster {posterNumber}",
+                        ImageSrc = $"/images/MoviePoster/Poster{posterNumber}.jpg",
+                        Tags = new List<string> { "action", "drama" }
+                    };
+                    posterViewModels.Add(viewModel);
+                }
+            }
+            posterViewModels.AddRange(newPosterInListOfPostersViewModels);
+            return View(posterViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePoster()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePoster(PosterCreationViewModel viewModel)
+        {
+            var poster = new PosterViewModel
+            {
+                Name = viewModel.Name,
+                ImageSrc = viewModel.Url,
+                Tags = viewModel.Tags,
+            };
+            newPosterInListOfPostersViewModels.Add(poster);
+
+            return RedirectToAction("AllPosters");
+        }
+
+    }
+}
---
+++ Net18Online/WebPortalEverthing/Models/MoviePoster/PosterCreationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPortalEverthing.Models.MoviePoster
+{
+    public class PosterCreationViewModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
