c4324b3b640018779076c8798a6a6a23553b7a25
Стас Тростянко <lepeqh@gmail.com>
2024-10-22 23:39:58 +0300

    Change 0.1

--- Net18Online/Everything.Data.Fake/Repositories/AnimeCatalogRepository.cs
+++ Net18Online/Everything.Data.Fake/Repositories/AnimeCatalogRepository.cs
@@ -6,35 +6,35 @@ namespace Everything.Data.Fake.Repositories
 {
     public class AnimeCatalogRepository : IAnimeCatalogRepository
     {
-        private List<IAnimeCatalogData> animes = new List<IAnimeCatalogData>();
+        private List<IAnimeCatalogData> _animes = new List<IAnimeCatalogData>();
 
         public void Add(IAnimeCatalogData data)
         {
-            data.Id = animes.Any()
-                ? animes.Max(x => x.Id) + 1
+            data.Id = _animes.Any()
+                ? _animes.Max(x => x.Id) + 1
                 : 1;
 
-            animes.Add(data);
+            _animes.Add(data);
         }
 
         public void Delete(IAnimeCatalogData data)
         {
-            animes.Remove(data);
+            _animes.Remove(data);
         }
 
         public List<IAnimeCatalogData> GetAll()
         {
-            return animes;
+            return _animes;
         }
 
         public IAnimeCatalogData? Get(int id)
         {
-            return animes.FirstOrDefault(x => x.Id == id);
+            return _animes.FirstOrDefault(x => x.Id == id);
         }
 
         public bool Any()
         {
-            return animes.Any();
+            return _animes.Any();
         }
     }
 }
