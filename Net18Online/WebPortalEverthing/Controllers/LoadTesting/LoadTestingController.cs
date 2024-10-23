fb5cd1913f4bb431252082961a9dabc495cd2be0
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-10-23 18:04:30 +0300

    refactor

--- Net18Online/WebPortalEverthing/Controllers/LoadTesting/LoadTestingController.cs
+++ Net18Online/WebPortalEverthing/Controllers/LoadTesting/LoadTestingController.cs
@@ -5,13 +5,15 @@ namespace WebPortalEverthing.Controllers.LoadTesting
 {
     public class LoadTestingController : Controller
     {
+        private static LoadTestingContentMetricsListViewModel model = new LoadTestingContentMetricsListViewModel(10);
+
         public IActionResult ContenMetricsListView(
             decimal throughput,
-            decimal average,
-            int CountMetrics)//входные параметры приходят снаружи от пользователя страницы
+            decimal average)
+         /*  , int countMetrics)//входные параметры приходят снаружи от пользователя страницы */
         {
             // Инициализация модели
-            var model = new LoadTestingContentMetricsListViewModel(CountMetrics);
+            
             model.Metrics[0].Throughput = throughput;
             model.Metrics[0].Average = average;
 
@@ -19,8 +21,32 @@ namespace WebPortalEverthing.Controllers.LoadTesting
             return View(model); //model выдаст данные наружу, на страницу
         }
 
+        [HttpGet]
         public IActionResult CreateProfile()
         {
+            Guid guid;
+            string name;
+            decimal throughput;
+            decimal average;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProfile
+            (string name,
+            decimal throughput,
+            decimal average)
+        {
+            Guid guid;
+
+            new Metric
+            {
+                Guid = Guid.NewGuid(),
+                Name = name,
+                Throughput = throughput * 1.0m,
+                Average = average * 1.0m
+            };
             return View();
         }
     }
