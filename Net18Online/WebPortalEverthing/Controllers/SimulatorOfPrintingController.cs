d3bcda0d1ec39673ca9194e53a26d62e900bcb20
Antonina765 <antoninakapuza0@gmail.com>
2024-10-25 23:11:32 +0300

    replace array to List and add controller viewModel and cshtml

---
+++ Net18Online/WebPortalEverthing/Controllers/SimulatorOfPrintingController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimulatorOfPrinting.Models.TextProvider;
+using WebPortalEverthing.Models.SimulatorOfPrinting;
+
+namespace WebPortalEverthing.Controllers
+{
+    public class SimulatorOfPrintingController : Controller
+    {
+        private TextProvider _textProvider;
+
+        public SimulatorOfPrintingController(TextProvider textProvider)
+        {
+            _textProvider = textProvider;
+        }
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        [HttpGet]
+        public IActionResult TextToTypping()
+        {
+            var model = new TypingViewModel
+            {
+                CurrentText = textProvider.GetText(),
+                UserInput = string.Empty,
+                Result = string.Empty
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult TextToTypping(TypingViewModel model)
+        {
+            stopwatch.Stop();
+            model.ElapsedTime = stopwatch.Elapsed;
+
+            if (model.UserInput == model.CurrentText)
+            {
+                model.Result = "Correct!";
+            }
+            else
+            {
+                model.Result = "Incorrect!";
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult StartTimer()
+        {
+            stopwatch.Start();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult StopTimer()
+        {
+            stopwatch.Stop();
+            return Ok();
+        }
+    }
+}
