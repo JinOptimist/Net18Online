410a0a22be90795084a3b2b06f47fea4aaf6204a
Ilya Klunik <kila27643@gmail.com>
2024-11-02 23:13:07 +0300

    Add a page for chess

---
+++ Net18Online/WebPortalEverthing/Models/Chess/PartiesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPortalEverthing.Models.Chess
+{
+    public class PartiesViewModel
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Winner { get; set; }
+    }
+}
