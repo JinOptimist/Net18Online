224de994683bc6c86cabb5106af270b66e880324
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-11-03 18:35:41 +0300

    add repository real

--- Net18Online/Everything.Data.Interface/Repositories/ILoadTestingRepository.cs
+++ Net18Online/Everything.Data.Interface/Repositories/ILoadTestingRepository.cs
@@ -3,12 +3,19 @@ using Everything.Data;
 
 namespace Everything.Data.Interface.Repositories
 {
-    public interface ILoadTestingRepository
+    public interface ILoadTestingRepository<T> : IBaseRepository<T>
+        where T : IMetricData
     {
         void Add(IMetricData data);
         void Delete(IMetricData data);
         List<IMetricData> GetAll();
         IMetricData? Get(Guid Guid);
         bool Any();
+
+        IEnumerable<T> GetMostLoaded();
+
+        void UpdateName(Guid Guid, string newName);
+
+        void UpdateImage(Guid Guid, string newThroughput);
     }
 }
---
+++ Net18Online/Everything.Data/Repositories/LoadTestingRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Everything.Data.Interface.Models;
+using Everything.Data.Interface.Repositories;
+using Everything.Data.Models;
+
+namespace Everything.Data.Repositories
+{
+    public interface ILoadTestingRepositoryReal : ILoadTestingRepository<MetricData>
+    {
+    }
+    public class LoadTestingRepository : ILoadTestingRepositoryReal
+    {
+        private WebDbContext _webDbContext;
+
+        public LoadTestingRepository(WebDbContext webDbContext)
+        {
+            _webDbContext = webDbContext;
+        }
+
+        public void Add(MetricData data)
+        {
+            _webDbContext.Add(data);
+            _webDbContext.SaveChanges();
+        }
+
+        public bool Any()
+        {
+            return _webDbContext.Metrics.Any();
+        }
+
+        public void Delete(MetricData data)
+        {
+            _webDbContext.Metrics.Remove(data);
+            _webDbContext.SaveChanges();
+        }
+
+        public void Delete(Guid Guid)
+        {
+            var data = Get(Guid);
+            Delete(data);
+        }
+
+        public MetricData? Get(int id)
+        {
+            return _webDbContext.Metrics.FirstOrDefault(x => x.Id == id);
+        }
+        public MetricData? Get(Guid Guid)
+        {
+            return _webDbContext.Metrics.FirstOrDefault(x => x.Guid == Guid);
+        }
+
+        public IEnumerable<MetricData> GetAll()
+        {
+            return GetFinilizeMetric().ToList();
+        }
+
+        public IEnumerable<MetricData> GetMostPopular()
+        {
+            return GetFinilizeMetric()
+                .Take(3)
+                .OrderByDescending(x => x.Id)
+                .ToList();
+        }
+
+        public void UpdateImage(int id, string url)
+        {
+            var Metric = _webDbContext.Metrics.First(x => x.Id == id);
+
+            Metric.ImageSrc = url;
+
+            _webDbContext.SaveChanges();
+        }
+
+        public void UpdateName(int id, string newName)
+        {
+            var Metric = _webDbContext.Metrics.First(x => x.Id == id);
+
+            Metric.Name = newName;
+
+            _webDbContext.SaveChanges();
+        }
+
+        private IQueryable<MetricData> GetFinilizeMetric()
+        {
+            return _webDbContext
+                .Metrics
+                .Where(x => !string.IsNullOrEmpty(x.ImageSrc));
+        }
+    }
+}
