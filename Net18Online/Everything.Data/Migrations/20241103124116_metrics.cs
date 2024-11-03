6666227a8ce4e5733318071581e7aea5b82cdad3
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-11-03 15:43:12 +0300

    add migration metrics table

---
+++ Net18Online/Everything.Data/Migrations/20241103124116_metrics.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Everything.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class metrics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Metrics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Throughput = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Average = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Metrics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Metrics");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Games",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
