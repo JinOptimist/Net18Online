45d8ff7f5362e031ac59e989b390343057aa3e0a
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-11-05 21:57:51 +0300

    reload id into metric model

--- Net18Online/Everything.Data/Models/MetricData.cs
+++ Net18Online/Everything.Data/Models/MetricData.cs
@@ -4,6 +4,8 @@ namespace Everything.Data.Models
 {
     public class MetricData : BaseModel, IMetricData
     {
+
+        public int Id { get; set; }
         public Guid Guid { get; set; } = Guid.NewGuid(); // Инициализация Guid при создании новой метрики, запись вместо описания в конструкторе, тоже самое д.б.
         public string Name { get; set; }
         public decimal Throughput { get; set; }
