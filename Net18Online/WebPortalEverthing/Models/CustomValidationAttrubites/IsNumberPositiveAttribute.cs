6dcc3928f1c5389ee111b09e3b707334adbe50aa
glytop <123397208+glytop@users.noreply.github.com>
2024-11-10 20:28:15 +0300

    add validation attribute for coffe shop

---
+++ Net18Online/WebPortalEverthing/Models/CustomValidationAttrubites/IsNumberPositiveAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPortalEverthing.Models.CustomValidationAttrubites
+{
+    public class IsNumberPositiveAttribute : ValidationAttribute
+    {
+        public override string FormatErrorMessage(string name)
+        {
+            return string.IsNullOrEmpty(ErrorMessage)
+                ? "Значение не может быть отрицательнным"
+                : ErrorMessage;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value is null)
+            {
+                return false;
+            }
+
+            if (value is not decimal decimalValue)
+            {
+                return false;
+            }
+
+            if (decimalValue < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
