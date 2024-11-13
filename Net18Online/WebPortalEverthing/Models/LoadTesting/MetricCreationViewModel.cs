59e72fa4baab3dbd960cd4974322c5ca80a8e633
Терехова Светлана Александровна <svetlana.terekhova@omnichannel.ru>
2024-11-13 22:00:38 +0300

    add attribute

---
+++ Net18Online/WebPortalEverthing/Models/LoadTesting/MetricCreationViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using WebPortalEverthing.Models.LoadTesting.TestingAttributes;
+
+namespace WebPortalEverthing.Models.LoadTesting
+{
+    public class MetricCreationViewModel
+    {
+    //    [UniqMetricId]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100), MinLength(3,ErrorMessage ="Name must be between 3 and 100 characters")]
+        [UniqMetricName]
+        public string Name { get; set; }
+
+        [Required]
+        Guid Guid { get; set; } = Guid.NewGuid(); // Инициализация Guid при создании новой метрики, вместо описания в конструкторе
+
+        [Required]
+        [ZeroUpAttribute]
+        public decimal Throughput { get; set; }
+
+        [Required]
+        [ZeroUpAttribute]
+        [IsCorrectAverageAttribute]
+        public decimal Average { get; set; }
+    }
+}
