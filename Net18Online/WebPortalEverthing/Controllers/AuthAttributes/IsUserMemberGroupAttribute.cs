6be6a051e512df1b3ba0cfeee2aca2cf1574324a
KozulevDV <den1119997@yandex.ru>
2024-11-20 17:18:23 +0300

    Prevent user without group "SurveysCreatorOrEditor" from creating new surveys

---
+++ Net18Online/WebPortalEverthing/Controllers/AuthAttributes/IsUserMemberGroupAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Enums.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebPortalEverthing.Services;
+
+namespace WebPortalEverthing.Controllers.AuthAttributes
+{
+    public class IsUserMemberGroupAttribute : ActionFilterAttribute
+    {
+        private Role _role;
+        public IsUserMemberGroupAttribute(Role role)
+        {
+            _role = role;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var authService = context
+                .HttpContext
+                .RequestServices
+                .GetRequiredService<AuthService>();
+
+            if (!authService.IsUserMemberGroup(_role))
+            {
+                context.Result = new ForbidResult();
+                return;
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
