1e3309d3e46ff1f5be8f2ac3e8a21e64917d0112
glytop <123397208+glytop@users.noreply.github.com>
2024-11-25 19:20:02 +0300

    delete unnecessary argument

--- Net18Online/WebPortalEverthing/Models/CoffeShop/Profile/ProfileViewModel.cs
+++ Net18Online/WebPortalEverthing/Models/CoffeShop/Profile/ProfileViewModel.cs
@@ -1,12 +1,9 @@
-﻿using WebPortalEverthing.Models.CustomValidationAttrubites;
-
-namespace WebPortalEverthing.Models.CoffeShop.Profile
+﻿namespace WebPortalEverthing.Models.CoffeShop.Profile
 {
     public class ProfileViewModel
     {
         public string UserName { get; set; }
 
-        [AllowedExtensions([".jpg", ".png"])]
         public string ProfileAvatar { get; set; }
 
         public List<CoffeShortViewModel> Coffe { get; set; }
