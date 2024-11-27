0c5987a89b0bee089aa97fdc61dfe4ea78dc295c
Antonina765 <antoninakapuza0@gmail.com>
2024-11-27 18:23:00 +0300

    add more localization

--- Net18Online/WebPortalEverthing/Models/Ecology/PostCreationViewModel.cs
+++ Net18Online/WebPortalEverthing/Models/Ecology/PostCreationViewModel.cs
@@ -12,15 +12,21 @@ namespace WebPortalEverthing.Models.Ecology
         //[IsUrl(ErrorMessage = "This URL is invalid")]
         public string Url { get; set; }
         
-        [Required(ErrorMessage = "Text is required.")]
+        [Required(
+            ErrorMessageResourceType = typeof(Localizations.Ecology), 
+            ErrorMessageResourceName = "ValidationMessage_RequiredText")]
+        //[Required(ErrorMessage = "Text is required.")]
         [EcologyText, IsCorrectLength(15)] 
         public string Text { get; set;}
+    
         public int PostId { get; set; }
         public List<SelectListItem>? Posts { get; set; }
-        
+
         [MaxFileSize(52428800)]
-        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "Please upload a valid image file (jpg, jpeg, png).")]
+        [FileExtensions(Extensions = "jpg,jpeg,png", 
+            ErrorMessageResourceType = typeof(Localizations.Ecology), 
+            ErrorMessageResourceName = "ValidationMessage_UploadFile")]
+        //[FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "Please upload a valid image file (jpg, jpeg, png).")]
         public IFormFile ImageFile { get; set; }
-        
     }
 }
