15bc3067737eb22eb2e1e48b1efc96d5de21a57c
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-12-02 21:47:07 +0300

    Add first example of JS

---
+++ Net18Online/WebPortalEverthing/wwwroot/js/AnimeGirl/AllGirls.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+  $(".girl .toggle-tags").click(function () {
+    $(this)
+      .closest(".girl") // tag .girl was founded
+      .find(".tag-container")
+      .toggle(1000);
+
+    $(".girls").removeClass("highlight-is-active");
+  });
+
+  $(".girl .image-container img").click(function () {
+    $(this).closest(".image-container").toggleClass("full");
+  });
+
+  $(".girl .tag").click(function () {
+    const tagValue = $(this).text();
+
+    // $(".tag:contains('" + tagValue +"')")
+    const theSameTags = $(`.tag:contains('${tagValue}')`);
+
+    $(".girl").removeClass("highlighted");
+
+    theSameTags.each((index, elem) => {
+      $(elem).closest(".girl").addClass("highlighted");
+    });
+
+    $(".girls").addClass("highlight-is-active");
+  });
+});
--- Net18Online/WebPortalEverthing/wwwroot/js/site.js
+++ Net18Online/WebPortalEverthing/wwwroot/js/site.js
@@ -1,4 +1,62 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var nameV1 = "Ivan"; // var is OUTDATED
+var nameV2 = "Ivan"; // var is OUTDATED
 
-// Write your JavaScript code.
+let nameV3 = "Ivan";
+let nameV4; // undefined
+
+const nameV5 = "Ivan"; // some sort of readonly
+
+let age = 15; // number
+let chanceToWin = 0.5; // number
+
+let isMan = true;
+
+let userV1 = {
+  name: nameV2,
+  age: age,
+  isMan: isMan,
+};
+
+let userV2 = {
+  name: "Lera",
+  age: 17,
+  isAdult: false,
+};
+
+function DoFun() {
+  console.log("Hi");
+}
+
+function Calc(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+let CalcV2 = function (firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+};
+
+let CalcV3 = (firstNumber, secondNumber) => {
+  return firstNumber + secondNumber;
+};
+
+CalcV2(1, 5);
+Calc(5, 9);
+
+function unusualFunction(user, favMethod) {
+  console.log(user.name);
+  favMethod(user.age);
+}
+
+function checkIsAdult(age) {
+  return age > 18;
+}
+
+function sayMyAge(age) {
+  console.log(age);
+}
+
+unusualFunction(userV1, checkIsAdult);
+unusualFunction(userV2, sayMyAge);
+unusualFunction(userV2, function (a) {
+  return a * a;
+});
