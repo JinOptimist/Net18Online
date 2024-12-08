620a51002d85771974a96ac9e55fa04407eee7fe
Света <svetlana@test.com>
2024-12-08 19:41:37 +0300

    add toggle button

--- Net18Online/WebPortalEverthing/wwwroot/js/LoadTesting/metrics.js
+++ Net18Online/WebPortalEverthing/wwwroot/js/LoadTesting/metrics.js
@@ -1,1 +1,28 @@
-﻿alert(`Приветственный алерт.`);
+﻿//alert("Hellow Russian");
+$(".discription-type .type-container img").click(function () {
+  console.log("Hellow/==== Russian");
+});
+
+$(".toggle-button-tag-additional-info").click(function () {
+  console.log("toggle metrics");
+  $(this) //элемент, по которому кликнули
+    .closest(".discription-type") //родителя нашли
+    .find(".additional-info .tag") //у родителя нашли по селектору элемент
+    .slideToggle(); // Используем slideToggle для плавного появления/скрытия элементов
+});
+
+// $(".discription-type .type-container img").click(function () {
+// .css("display", "inline-block"); //сменили у найденного элемента скрытое(none) отображение на видимое
+//покажи// .show();
+//спрятать// hide();
+//было // $(".discription-type .additional-info .tag").css("display", "inline-block");
+
+/*$(document).ready(function () {
+    $(".discription-type .type-container img").click(function () {
+        console.log("Hellow/==== Russian");
+    $(this) //элемент, по которому кликнули
+      .closest(".discription-type") //родителя нашли
+      .find(".additional-info .tag") //у родителя нашли по селектору элемент
+      .slideToggle(); // Используем slideToggle для плавного появления/скрытия элементов
+  });
+});*/
