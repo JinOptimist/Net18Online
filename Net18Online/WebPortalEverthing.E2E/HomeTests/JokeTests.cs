f1f8f029b1ff29c78341e44ecf22c4b7b2065734
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2024-12-18 21:06:41 +0300

    Add waiter for e2e test

---
+++ Net18Online/WebPortalEverthing.E2E/HomeTests/JokeTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using WebPortalEverthing.E2E.Pages.Home;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace WebPortalEverthing.E2E.HomeTests
+{
+    public class JokeTests
+    {
+        public IWebDriver _webDriver;
+        public WebDriverWait _wait; 
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _webDriver = new ChromeDriver();
+            _wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(10));
+            _webDriver.Url = CommonConstants.BASE_URL;
+        }
+
+        [Test]
+        public void Test()
+        {
+            var jokeButton  = _wait.Until(
+                ExpectedConditions.ElementToBeClickable(IndexSelectors.JokeButton));
+
+            jokeButton.Click();
+
+            Assert.Pass();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _webDriver.Close();
+        }
+    }
+}
---
+++ Net18Online/WebPortalEverthing.E2E/Pages/Home/IndexSelectors.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace WebPortalEverthing.E2E.Pages.Home
+{
+    internal class IndexSelectors
+    {
+        public static By JokeButton = By.CssSelector(".joke button");
+    }
+}
