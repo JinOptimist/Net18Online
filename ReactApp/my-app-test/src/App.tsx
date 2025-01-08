d920bda1783b3dd65ff6eed992741dae9cc11e56
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-01-08 19:51:15 +0300

    Create empty React app

---
+++ ReactApp/my-app-test/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Smile from './components/Smile';
+
+function App() {
+  return (
+    <div className="App">
+      <Smile></Smile>
+    </div>
+  );
+}
+
+export default App;
