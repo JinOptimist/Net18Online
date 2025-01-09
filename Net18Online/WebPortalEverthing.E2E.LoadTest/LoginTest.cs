ab2b8b93238165b8afec61fb106d9f5188303103
Света <svetlana@test.com>
2025-01-09 20:28:06 +0300

    Neget: Selenium.WebDriver Selenium.WebDriver.ChromeDriver WebDriverManager для  автоматического скачивания версии ChromeDriver, соответствующую установленному браузеру.

--- Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
+++ Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
@@ -1,4 +1,11 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//нужно установить пакет WebDriverManager
+//Install-Package WebDriverManager
+
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace WebPortalEverthing.E2E.LoadTest
 {
@@ -7,6 +14,22 @@ namespace WebPortalEverthing.E2E.LoadTest
         [Test]
         public void Login()
         {
+            // Автоматически устанавливает правильную версию ChromeDriver
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            //// <summary>
+            ///Можно вручную скачать драйвер и прописать к нему путь
+            /// var chromeDriverPath = @"C:\Path\To\ChromeDriver"; // Укажите путь к chromedriver.exe
+            ///var driver = new ChromeDriver(chromeDriverPath);
+            /// </summary>
+
+            using (var webDriver = new ChromeDriver())
+            {
+                webDriver.Navigate().GoToUrl(
+
+                "https://localhost:7130/LoadTesting/ContenMetricsListView");
+            }
+
             Assert.Pass();
         }
     }
