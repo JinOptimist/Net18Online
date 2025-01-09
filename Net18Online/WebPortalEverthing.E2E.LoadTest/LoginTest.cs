77bce5f8c85d654a88c59ba98134cae141a6897c
Света <svetlana@test.com>
2025-01-09 19:48:30 +0300

    Создать свой End To End тест. Который хотя бы один элемент создаёт и затем за собой удаляет 27 video Nuget: NUnit ядро тестов NUnit3TestAdapter для запуска тестов Microsoft.Net.Test.Sdk для запуска тестов в VS

---
+++ Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace WebPortalEverthing.E2E.LoadTest
+{
+    public class LoginTest
+    {
+        [Test]
+        public void Login()
+        {
+            Assert.Pass();
+        }
+    }
+}
