180df730b52d4679d4bb707fadf520544a15e968
Света <svetlana@test.com>
2025-01-10 21:05:26 +0300

    add presetup and post condotion

--- Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
+++ Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Internal.Commands;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -14,39 +15,57 @@ namespace WebPortalEverthing.E2E.LoadTest
 {
     public class LoginTest
     {
-        [Test]
-        public void LoginWithValidCredentials()
+        private IWebDriver webDriver;
+
+        //[SetUp] Before each test
+        [OneTimeSetUp]
+        public void Setup()
         {
             // Установка драйвера
             new DriverManager().SetUpDriver(new ChromeConfig());
 
-            using (var webDriver = new ChromeDriver())
-            {
-                // Открытие страницы
-                webDriver.Navigate().GoToUrl("https://localhost:7130/LoadAuth/LoginLoadUserView");
+            webDriver = new ChromeDriver();
+        }
 
-                // Явное ожидание
-                var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+        [Test]
+        public void LoginWithValidCredentials()
+        {
 
-                // Поиск поля логина
-                var loginDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".login")));
-                var loginInput = loginDiv.FindElement(By.TagName("input"));
 
-                // Поиск поля пароля
-                var passwordDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".password")));
-                var passwordInput = passwordDiv.FindElement(By.TagName("input"));
+            // Открытие страницы
+            webDriver.Navigate().GoToUrl("https://localhost:7130/LoadAuth/LoginLoadUserView");
 
-                // Ввод данных
-                loginInput.SendKeys("qa3");
-                passwordInput.SendKeys("qa3");
+            // Явное ожидание
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
 
-                // Поиск и нажатие на кнопку
-                var signInButton = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("button[type='submit']")));
-                signInButton.Click();
+            // Поиск поля логина
+            var loginDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".login")));
+            var loginInput = loginDiv.FindElement(By.TagName("input"));
 
-                // Проверка успешной авторизации (пример: URL содержит 'Home')
-                Assert.That(wait.Until(ExpectedConditions.UrlContains("IndexLoadVolumeView")));
-            }
+            // Поиск поля пароля
+            var passwordDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".password")));
+            var passwordInput = passwordDiv.FindElement(By.TagName("input"));
+
+            // Ввод данных
+            loginInput.SendKeys("admin");
+            passwordInput.SendKeys("admin");
+
+            // Поиск и нажатие на кнопку
+            var signInButton = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("button[type='submit']")));
+            signInButton.Click();
+
+            // Проверка успешной авторизации (пример: URL содержит 'Home')
+            Assert.That(wait.Until(ExpectedConditions.UrlContains("IndexLoadVolumeView")));
+
+
+
+            //    [TearDown]//after each test
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            webDriver.Close();
         }
     }
 }
