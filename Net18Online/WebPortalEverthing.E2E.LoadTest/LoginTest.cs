2efd615a73d80c1f47a6cca58e594a4979f162f3
Света <svetlana@test.com>
2025-01-10 19:53:28 +0300

    add Test Login

--- Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
+++ Net18Online/WebPortalEverthing.E2E.LoadTest/LoginTest.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
 //нужно установить пакет WebDriverManager
 //Install-Package WebDriverManager
 
@@ -12,9 +15,9 @@ namespace WebPortalEverthing.E2E.LoadTest
     public class LoginTest
     {
         [Test]
-        public void Login()
+        public void LoginWithValidCredentials()
         {
-            // Автоматически устанавливает правильную версию ChromeDriver
+            // Установка драйвера
             new DriverManager().SetUpDriver(new ChromeConfig());
 
             //// <summary>
@@ -25,12 +28,31 @@ namespace WebPortalEverthing.E2E.LoadTest
 
             using (var webDriver = new ChromeDriver())
             {
-                webDriver.Navigate().GoToUrl(
+                // Открытие страницы
+                webDriver.Navigate().GoToUrl("https://localhost:7130/LoadAuth/LoginLoadUserView");
 
-                "https://localhost:7130/LoadTesting/ContenMetricsListView");
+                // Явное ожидание
+                var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+
+                // Поиск поля логина
+                var loginDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".login")));
+                var loginInput = loginDiv.FindElement(By.TagName("input"));
+
+                // Поиск поля пароля
+                var passwordDiv = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".password")));
+                var passwordInput = passwordDiv.FindElement(By.TagName("input"));
+
+                // Ввод данных
+                loginInput.SendKeys("qa3");
+                passwordInput.SendKeys("qa3");
+
+                // Поиск и нажатие на кнопку
+                var signInButton = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("button[type='submit']")));
+                signInButton.Click();
+
+                // Проверка успешной авторизации (пример: URL содержит 'Home')
+                Assert.That(wait.Until(ExpectedConditions.UrlContains("IndexLoadVolumeView")));
             }
-
-            Assert.Pass();
         }
     }
 }
