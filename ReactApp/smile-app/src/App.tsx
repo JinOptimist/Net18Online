c73839cb47c4b18d816e6c581ae5a967786fa832
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2025-01-13 19:26:07 +0300

    Add React with Vite

---
+++ ReactApp/smile-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Girls from './components/Girls'
+
+function App() {
+  return (
+    <>
+      <Girls />
+    </>
+  )
+}
+
+export default App
---
+++ ReactApp/smile-app/src/components/Girls.tsx
@@ -0,0 +1,5 @@
+function Girls() {
+  return <div>Girls</div>;
+}
+
+export default Girls;
