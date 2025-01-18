84657d3f6a0002cd942aca06cde05bc61ecd8df3
Света <svetlana@test.com>
2025-01-18 20:04:20 +0300

    add references

--- Net18Online/ReflectionLoadTesting/Program.cs
+++ Net18Online/ReflectionLoadTesting/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System.Reflection;
 
+/// <summary>
+/// передаём любой тип и получаем его сборку
+/// </summary>
+var type = typeof(WebPortalE);
+var assembly = type.Assembly;
+
 var assembly = Assembly.LoadFrom(@"C:\Users\svetlana.terekhova\source\repos\Net18Online\Net18Online\ReflectionLoadTest\bin\Debug\net6.0\ReflectionLoadTest.dll");
 
 var type = assembly.GetType("ReflectionLoadTest.Program");
