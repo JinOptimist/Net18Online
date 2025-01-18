2f8ecbe952d6199dbce14f1b547b52ba73447e98
Света <svetlana@test.com>
2025-01-18 19:57:05 +0300

    restore notmy

--- Net18Online/ReflectionLoadTesting/Program.cs
+++ Net18Online/ReflectionLoadTesting/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+var assembly = Assembly.LoadFrom(@"C:\Users\svetlana.terekhova\source\repos\Net18Online\Net18Online\ReflectionLoadTest\bin\Debug\net6.0\ReflectionLoadTest.dll");
+
+var type = assembly.GetType("ReflectionLoadTest.Program");
+
+
+Console.WriteLine(assembly);
+Console.WriteLine(type);
