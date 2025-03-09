6976204874c0cc938ae2042e1fe9723386626d68
glytop <123397208+glytop@users.noreply.github.com>
2025-03-09 20:22:56 +0300

    validation attribute

--- Net18Online/WebPortalEverthing/Models/Brand/CreateBrandViewModel.cs
+++ Net18Online/WebPortalEverthing/Models/Brand/CreateBrandViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace WebPortalEverthing.Models.Brand
+﻿using System.ComponentModel.DataAnnotations;
+using WebPortalEverthing.Models.CustomValidationAttrubites;
+
+namespace WebPortalEverthing.Models.Brand
 {
     public class CreateBrandViewModel
     {
+        [UniqBrand]
         public string Name { get; set; }
-
+        [Required]
         public string Company { get; set; }
     }
 }
